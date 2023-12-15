021e521e2ef0e58cd2c8a0a62e7abdd96d13af7f
MohJeliAlmutaali <114728937+MohJeliAlmutaali@users.noreply.github.com>
2023-12-15 15:51:06 +0700

    initial commit

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../models/db'); // Assuming the correct path to your db module
+const db = require('../models/db');
 
-// GET all mahasiswa
+// GET mahasiswa
 router.get('/', (req, res) => {
   db.query('SELECT * FROM mahasiswa', (error, results) => {
     if (error) {
@@ -14,7 +14,7 @@ router.get('/', (req, res) => {
   });
 });
 
-// GET specific mahasiswa by nim
+// GET /mahasiswa/:nim
 router.get('/:nim', (req, res) => {
   const mahasiswaNim = req.params.nim;
   db.query('SELECT * FROM mahasiswa WHERE nim = ?', [mahasiswaNim], (error, results) => {
@@ -29,7 +29,7 @@ router.get('/:nim', (req, res) => {
   });
 });
 
-// PUT update mahasiswa by nim
+// PUT /mahasiswa/:nim
 router.put('/:nim', (req, res) => {
   const mahasiswaNim = req.params.nim;
   const { nama, gender, prodi, alamat } = req.body;
@@ -41,10 +41,45 @@ router.put('/:nim', (req, res) => {
         console.error('Error updating mahasiswa:', error);
         res.status(500).json({ message: 'Internal Server Error' });
       } else {
-        res.json({ message: 'Updating mahasiswa successfully' });
+        res.json("Updating mahasiswa successfully");
       }
     }
   );
+});
+
+router.post('/', (req, res) => {
+    const newMahasiswa = req.body;
+
+    // Pastikan req.body berisi data yang sesuai
+    if (!newMahasiswa || typeof newMahasiswa !== 'object') {
+        res.status(400).json({ message: 'Invalid request body' });
+        return;
+    }
+
+    db.query("INSERT INTO mahasiswa SET ?", newMahasiswa, (error, results) => {
+        if (error) {
+            console.error(`Error creating mahasiswa: ${error}`);
+            res.status(500).json({ message: 'Internal Server Error' });
+        } else {
+            res.status(200).json({ message: 'Mahasiswa created successfully'});
+        }
+    });
+});
+
+router.delete('/:nim', (req, res) => {
+    const mahasiswa = req.body;
+    const nama = mahasiswa.nama
+    const mahasiswaDeleteNIM = req.params.nim;
+    db.query('DELETE FROM mahasiswa WHERE nim = ?', [mahasiswaDeleteNIM], (error, results) => {
+        if (error) {
+            console.error("Error deleting mahasiswa", error);
+            res.status(500).json({ message: 'Internal Server Error' });
+        } else if (results.affectedRows === 0) {
+            res.status(404).json({ message: 'Mahasiswa not found' });
+        } else {
+            res.status(200).json({ message: `Mahasiswa ${nama}deleted successfully` });
+        }
+    });
 });
 
 module.exports = router;
